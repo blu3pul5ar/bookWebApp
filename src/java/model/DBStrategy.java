f34e56f16fffcc2043577c820007bb57b4ce484b
blu3pul5ar <npiette@my.wctc.edu>
2016-02-10 17:14:21 -0600

    fixed the dbstrategy

--- src/java/model/DBStrategy.java
+++ src/java/model/DBStrategy.java
@@ -9,6 +9,20 @@ package model;
  *
  * @author Nicholas
  */
+
+    import java.sql.SQLException;
+
+/**
+ * The general contract for all JDBC database access strategy implementations.
+ * Use of this interface is mandatory.
+ * 
+ * @author jlombardo
+ */
 public interface DBStrategy {
     
+    public abstract void openConnection(String driverClass, String url, 
+            String userName, String password) 
+            throws ClassNotFoundException, SQLException;
+    
+    public abstract void closeConnection() throws SQLException;
 }
